1f8ee77c7c0ccf5d23651a3110047f438895f5a5
GuhAlcantara <gustavoamv@gmail.com>
2025-02-23 16:31:12 -0300

    Adicionando matrizes

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,8 +4,7 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
 
-int main() {
-    // Nível Novato - Posicionamento dos Navios
+ // Nível Novato - Posicionamento dos Navios
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
@@ -35,6 +34,43 @@ int main() {
     // 0 0 1 0 0
     // 1 1 1 1 1
     // 0 0 1 0 0
+
+
+int main() {
+   
+
+
+    int tabuleiro[5][5];
+        tabuleiro[0][0] = 1;
+        tabuleiro[0][1] = 2;
+        tabuleiro[0][2] = 3; 
+        tabuleiro[0][3] = 4;
+        tabuleiro[0][4] = 5;
+        tabuleiro[1][0] = 6;
+        tabuleiro[1][1] = 7;
+        tabuleiro[1][2] = 8;
+        tabuleiro[1][3] = 9; 
+        tabuleiro[1][4] = 10; 
+        tabuleiro[2][0] = 11; 
+        tabuleiro[2][1] = 12; 
+        tabuleiro[2][2] = 13; 
+        tabuleiro[2][3] = 14; 
+        tabuleiro[2][4] = 15; 
+        tabuleiro[3][0] = 16; 
+        tabuleiro[3][1] = 17; 
+        tabuleiro[3][2] = 18; 
+        tabuleiro[3][3] = 19; 
+        tabuleiro[3][4] = 20; 
+        tabuleiro[4][0] = 21; 
+        tabuleiro[4][1] = 22; 
+        tabuleiro[4][2] = 23; 
+        tabuleiro[4][3] = 24; 
+        tabuleiro[4][4] = 25;  
+    
+   
+        
+    
+
 
     return 0;
 }
