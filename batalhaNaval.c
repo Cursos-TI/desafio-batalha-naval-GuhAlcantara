17482275c55816398e29a05fc73e381ee6a153ab
GuhAlcantara <Gustavoamv@gmail.com>
2025-02-24 03:01:09 +0000

    Tabuleiro montado

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,58 +4,61 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
 
- // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+// Nível Novato - Posicionamento dos Navios
+// Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
+// Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
+// Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+// Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
+// Sugestão: Expanda o tabuleiro para uma matriz 10x10.
+// Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
+// Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+// Nível Mestre - Habilidades Especiais com Matrizes
+// Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
+// Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
+// Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+// Exemplos de exibição das habilidades:
+// Exemplo para habilidade em cone:
+// 0 0 1 0 0
+// 0 1 1 1 0
+// 1 1 1 1 1
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+// Exemplo para habilidade em octaedro:
+// 0 0 1 0 0
+// 0 1 1 1 0
+// 0 0 1 0 0
 
+// Exemplo para habilidade em cruz:
+// 0 0 1 0 0
+// 1 1 1 1 1
+// 0 0 1 0 0
 
-int main() {
-   
+#define linha 10
+#define coluna 10
 
+int main()
+{
 
-    int tabuleiro[5][5] = {
-        {1,2,3,4,5},  
-        {6,7,8,9,10},  
-        {11,12,13,14,15},  
-        {16,17,18,19,20},  
-        {21,22,23,24,25}, 
-    };
+    int tabuleiro[linha][coluna];
+    int soma = 0;
+    int navio1 = 0, navio2 = 0, navio3 = 0, navio4 = 0;
 
-    printf("O navio na Horizontal está ocupando tres casas nas coordenadas [1][0],[1][1] ,[1][2] = %d , %d , %d\n",tabuleiro[1][0],tabuleiro[1][1], tabuleiro[1][2]);;
-    printf("O Navio na vertical está ocupando duas casas nas coordenadas [2][1] ,[3][1] = %d , %d\n" , tabuleiro[2][1] , tabuleiro[3][1]);
-        
-    
-   
-        
-    
+    for (int i = 0; i < linha; i++)
+    {
+        for (int j = 0; j < coluna; j++)
+        {
+            soma++;
+            tabuleiro[i][j] = 0;
+            printf("%d ", tabuleiro[i][j]);
+        }
 
+        printf("\n");
+    }
+    printf("O navio na Horizontal está ocupando tres casas nas coordenadas [1][0],[1][1] ,[1][2]\n");
+    ;
+    printf("O Navio na vertical está ocupando duas casas nas coordenadas [2][1] ,[3][1] = %d , %d\n", tabuleiro[2][1], tabuleiro[3][1]);
 
     return 0;
 }
